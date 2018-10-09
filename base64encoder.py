d465da249b3474eb9e4a2be983ca400b60e5f83d
Boy-314 <boy.314.yee.willie@gmail.com>
2018-10-09 16:04:39 -0400

    TODO: implement padding

--- base64encoder.py
+++ base64encoder.py
@@ -1,4 +1,18 @@
 fileName = input("Input file name: ");
 with open(fileName,"rb") as binary_file:
     data = binary_file.read();
-    print(data);
+lengthInput = len(data);
+bits = "";
+for i in range(lengthInput):
+    bits += '{0:08b}'.format(data[i]);
+splitBy = 6;
+splitBits = [bits[i:i+splitBy] for i in range(0,len(bits),splitBy)];
+base64 = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9","+","/"];
+lengthOutput = len(splitBits);
+output = "";
+for i in range(lengthOutput):
+    output += base64[int(splitBits[i],2)];
+outputFileName = fileName + ".BASE64";
+fileOutput = open(outputFileName,"w+");
+fileOutput.write(output);
+fileOutput.close();
