f17accb9d833305600f82e9ba21b13d5768c8c44
Boy-314 <boy.314.yee.willie@gmail.com>
2018-10-09 16:57:58 -0400

    october 9 2018

--- base64encoder.py
+++ base64encoder.py
@@ -9,9 +9,15 @@ splitBy = 6;
 splitBits = [bits[i:i+splitBy] for i in range(0,len(bits),splitBy)];
 base64 = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9","+","/"];
 lengthOutput = len(splitBits);
+if(len(splitBits[lengthOutput-1]) <= 6):
+    padding = 6 - len(splitBits[lengthOutput - 1]);
+    for i in range(padding):
+        splitBits[lengthOutput - 1] += "0";
 output = "";
 for i in range(lengthOutput):
     output += base64[int(splitBits[i],2)];
+addEquals = 0;
+
 outputFileName = fileName + ".BASE64";
 fileOutput = open(outputFileName,"w+");
 fileOutput.write(output);
